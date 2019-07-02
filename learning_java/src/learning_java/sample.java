3ebf676b973f0be4bb4e740416d51c44c7bb66f1
vkhambam <mnailu53@gmail.com>
2019-07-02 14:45:44 +0530

    sample

---
+++ learning_java/src/learning_java/sample.java
@@ -0,0 +1,16 @@
+package learning_java;
+
+public class sample {
+
+	public static void main(String[] args)
+	{
+		for (int j=1;j<=5;j++)
+		{
+			for (int i = 1; i <= j; i++) {
+				System.out.print(i +" ");
+			
+			}
+			System.out.println();
+		}
+	}
+}
